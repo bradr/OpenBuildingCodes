929e022c8dd03bfc82a786d4303cfc0064d182d8
bradr <bradroger@gmail.com>
2018-03-07 23:32:03 -0700

    Prettied up search and view pages

--- nginx/static/js/search.js
+++ nginx/static/js/search.js
@@ -1,18 +1,38 @@
 var query = getParam('query');
-var from = getParam('i');
-if (!query) {
-    query = 'building';
-}
+var p = getParam('p');
 $(document).ready(function() {
+    if (!query) {
+        window.location.href = '/';
+    }
+    if (!p) {
+        p = 1;
+    }
+    search(query,p);
+    
+    $('#search').on('submit',function(evt){
+        evt.preventDefault();
+        var searchTerm = $('#searchTerm').val();
+        //Sanitize the search term:
+        searchTerm = searchTerm.replace(/\s/g,'+');
+        window.history.pushState("Object", "", "search?query=" +searchTerm);
+        search(searchTerm,0);
+    });
+    $('#searchTerm').val(query);
+});
+
+
+function search(query, p) {
+    var from = p*10;
     var postdata = {
-        from: parseInt(from),
+        from: from,
         size: 10,
         query:{
             match:query
         },
         highlight: {
             fields: ['body']
-        }
+        },
+        fields: ["id", "fragments", "title", "by", "page"]
     };
     $.ajax({
         url: 'api/index.bleve/_search',
@@ -23,19 +43,70 @@ $(document).ready(function() {
             displayResults(results);
         }
     });
-});
+}
 
 function displayResults(results) {
     var outputs = [];
+    var pageOf = '';
+    if (p>1) {
+        pageOf = 'Page '+p+' of ';
+    }
+    $('#resultCount').html(pageOf + results.total_hits + ' Results');
     results.hits.forEach(function(i) {
-        outputs.push({title:i.id, fragment:i.fragments.body[0] });
+        outputs.push({title:i.id, fragment:i.fragments.body[0], fields:i.fields });
     });
     var template = $("#search-template").html();
     var template_output = Handlebars.compile(template);
-    $('#search_results').append(template_output(outputs))
+    $('#search_results').html(template_output(outputs));
+    
+    displayPages(results.total_hits);
 }
 
-
+function displayPages(hits) {
+    var pages = Math.ceil(hits/10);
+    var searchPages = '';
+    if (p>1) {
+        var url = window.location.href;
+        var newpage = parseInt(p)-1;
+        if (url.match('p=')) {
+            url = url.replace(/p=[^&]*/,'p='+newpage);
+        } else {
+            url = url+'&p='+newpage;
+        }
+        searchPages += '&nbsp;<a href="'+url+'"><b>\<</b></a>&nbsp;';
+    }
+    var start = 1;
+    if (p > 5) {
+        start = p-5;
+    }
+    if (p > (pages-5)) {
+        start = pages-10;
+    }
+    for (var i=start;i<pages;i++) {
+        if (i == p) {
+            searchPages += '&nbsp;' + i + '&nbsp;';
+        } else if (i < (start+10)) {
+            url = window.location.href;
+            if (url.match('p=')) {
+                url = url.replace(/p=[^&]*/,'p='+i);
+            } else {
+                url = url+'&p='+i;
+            }
+            searchPages += '&nbsp;<a href="' + url + '">' + i + '</a>&nbsp;';
+        }
+    }
+    if (p<(pages-1)) {
+        var url = window.location.href;
+        var nextpage = parseInt(p)+1;
+        if (url.match('p=')) {
+            url = url.replace(/p=[^&]*/,'p='+nextpage);
+        } else {
+            url = url+'&p='+nextpage;
+        }
+        searchPages += ' <a href="'+url+'"><b>\></b></a>';
+    }
+    $('#searchPages').html(searchPages);
+}
 
 //Get Parameters from URL
 function getParam(name, url) {
--- nginx/static/js/view.js
+++ nginx/static/js/view.js
@@ -1,10 +1,14 @@
+var json, show;
 $(document).ready(function() {
+  var query;
+  var referrer = document.referrer;
+  if (referrer.match(/search\?query=/gi)) {
+    query = getParam('query',document.referrer);
+  }
   var code = getParam('code');
   var page = getParam('page');
-  var show = getParam('show');
-  
-  var position = getParam('pos');
-  var json;
+  show = getParam('show');
+
   var filetype = "";
 
   //Fill in code data:
@@ -16,61 +20,135 @@ $(document).ready(function() {
     if (data.pdfurl) {
       filetype = 'pdf';
     }
+    $('#documentInfo').html(documentInfo());
     //Fill in HTML codes
     if (filetype == "html") {
       $.get('../../files/' + code + '.html', function(data) {
         $('#viewport').html(data);
-        //scroll to position if it exists
       });
     //Fill in PDF codes
     } else if (filetype == "pdf") {
       //Show the OCRed text
       if (show == 'txt') {
-        //$.get('../../files/' + code + '/' + code +  "_" + page + '.txt', function(data) {
-          text = data.body.replace(/\n/g, '<br />');
-          $('#viewport').html(text)
-          //scroll to position if it exists
-        //});
-        $('#ocr').text="PDF";
+        displayText(json.body);
       } else {
       //Show pdf page as .png
-        $('#viewport').html('<img src="../../files/' + code + '/img/' + code + '_' + page + '.png" style="max-width:100%;max-hight:auto" >');
+        displayPDF(code, page);
       }
     }
   });
+  
+  //Back Button:
+  if (query) {
+    $('#backButton').html('<button class="back btn btn-link btn-sm">Back to search results</button>');
+  }
 
   //Click handlers
-  $('#next').click(function() {
+  $('.next').click(function() {
+    $('.next').tooltip('hide');
     page = parseInt(page)+1;
-    window.history.pushState("hello", "", "view?code=" + code + "&page=" + page);
-    $('#viewport').html('<img src="../../files/' + code + '/'+code+'_' + page + '.png" style="max-width:100%;max-hight:auto" >');
+    next(code,page);
   });
-  $('#prev').click(function() {
+  $('.prev').click(function() {
+    $('.prev').tooltip('hide');
     page = parseInt(page)-1;
-    window.history.pushState("hello", "", "view?code=" + code + "&page=" + page);
-    $('#viewport').html('<img src="../../files/' + code + '/' + code + '_' + page + '.png" style="max-width:100%;max-height:auto" >');
+    prev(code,page);
   });
-  $('#ocr').click(function() {
+  $('#ocrButton').click(function() {
+    $('#ocrButton').tooltip('hide');
     if (show=='txt') {
-      window.history.pushState("hello", "", "view?code=" + code + "&page=" + page );
-      $('#ocr').text('Text');
-      $('#viewport').html('<img src="../../files/' +code + '/img/'+ code + '_' + page + '.png" style="max-width:100%;max-height:auto" >');
+      window.history.replaceState("Object", "", "view?code=" + code + "&page=" + page );
       show = "";
+      displayPDF(code,page);
     } else {
-      // $.get('../../files/'+code+ '/meta/' +code+ "_" +page+'.json', function(data) {
-      //   data = data.replace(/\n/g, '<br />');
-      //   window.history.pushState("hello", "", "view?code=" + code + "&page=" + page + "&show=txt");
-      //   $('#ocr').text('PDF');
-      //   $('#viewport').html(data);
-      //   show = "txt";
-      // });
-      window.history.pushState("hello", "", "view?code=" + code + "&page=" + page + "&show=txt");
-      $('#ocr').text('PDF');
-      $('#viewport').html(json.body.replace(/\n/g, '<br />'));
+      window.history.replaceState("Object", "", "/view?code=" + code + "&page=" + page + "&show=txt");
       show = "txt";
+      displayText(json.body);
     }
   });
+  $('.back').click(function() {
+    window.location.href = document.referrer;
+  });
+  $('#search').on('submit',function(evt){
+    evt.preventDefault();
+    var searchTerm = $('#searchTerm').val();
+    window.location.href ="/search?query=" +searchTerm;
+  });
+  
+  //Keypress handler:
+  $(document).on('keydown', function(e) {
+    if ($(e.target).is('input, textarea')) {
+      return;
+    } else {
+      if ( e.which === 34 || e.which === 39 || e.which === 74 ) {
+        page = parseInt(page)+1;
+        next(code,page);
+      } else if ( e.which === 33 || e.which === 37 || e.which === 75 ) {
+        page = parseInt(page)-1;
+        prev(code,page);
+      }
+    }
+  });
+  //Enable Tooltips:
+  $('[data-toggle="tooltip"]').tooltip()
 });
+
+function next(code,page) {
+  window.history.replaceState("Object", "", "view?code=" + code + "&page=" + page);
+  show = "";
+  displayPDF(code,page);
+  $.get('../../files/' + code + '/meta/' + code + '_'+page+'.json', function(data) {
+    json = data;
+  });
+  return page;
+}
+
+function prev(code,page) {
+  window.history.replaceState("Object", "", "view?code=" + code + "&page=" + page);
+  show = "";
+  displayPDF(code,page);
+  $.get('../../files/' + code + '/meta/' + code + '_'+page+'.json', function(data) {
+    json = data;
+  });
+  return page;
+}
+
+function displayText(text) {
+  $('#ocrButton').html('<img src="/img/file-pdf.svg" alt="PDF">');
+  $('#ocrButton').tooltip('dispose');
+  $('#ocrButton').tooltip({
+    title: 'View PDF Page',
+    delay: { show:500 },
+    placement: 'top',
+    trigger: 'hover'
+  });
+  text = text.replace(/\n\s*\n/g, '<br>\n');
+  text = text.replace(/\n/g, '<br>');
+  $('#viewport').html('<div class="ml-5 mr-5 mt-5 mb-5">' + text + '</div>');
+}
+
+function displayPDF(code,page) {
+  $('#ocrButton').html('<img src="/img/file-text.svg" alt="Text">');
+  $('#ocrButton').tooltip('dispose');
+  $('#ocrButton').tooltip({
+    title: 'View Document Text',
+    delay: { show:500 },
+    placement: 'top',
+    trigger: 'hover'
+  });
+  $('#viewport').html('<img src="../../files/' +code + '/img/'+ code + '_' + page + '.png" style="max-width:100%;max-height:auto" >');
+}
+
+function documentInfo() {
+  
+  var html = ' \
+    <div class="card"> \
+      <div class="ml-3 mt-3"> \
+        <dl><dt>Title</dt><dd>' + json.title + '</dd><dt>Author</dt><dd>' + json.by + '</dd><dt>Language</dt><dd>' + json.language + '</dd></dl> \
+      </div> \
+    </div>';
+  return html;
+}
 
 //Get Parameters from URL
 function getParam(name, url) {
